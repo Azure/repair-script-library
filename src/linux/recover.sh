be099958c6876df2beb9ba9a58b121582ca89017
Marcus Lachmanez <malachma@microsoft.com>
2020-04-01 14:56:53 +0200

    moved recovery scripts into the new folder src/linux/common/helpers

---
+++ src/linux/recover.sh
@@ -0,0 +1,22 @@
+. ./src/linux/common/setup/init.sh
+
+#We store our files in /tmp
+cd /tmp
+
+while true; do
+    wget -q --no-cache https://raw.githubusercontent.com/malachma/azure-auto-recover/master/base.sh
+    if [[ $? -eq 0 ]]; then
+        echo "File base.sh fetched"
+        break # the file got fetched, otherwise we try this again
+    fi
+    sleep 1
+done
+
+#execute the base.sh app
+Log-Output "Starting the recovery"
+chmod 700 /tmp/base.sh
+/tmp/base.sh $1 
+
+Log-Output "Recovery script finished"
+
+exit $STATUS_SUCCESS
