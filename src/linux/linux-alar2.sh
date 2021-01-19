17c5139f75e65a997f46217d213cc5ccbf44beb1
malachma <malachma@microsoft.com>
2021-01-19 17:39:03 +0100

    exit returns the error state from alar2

--- src/linux/linux-alar2.sh
+++ src/linux/linux-alar2.sh
@@ -5,4 +5,7 @@ Log-Output "Starting the recovery"
 cd ./src/linux/common/helpers/alar2
 chmod 700 ./bin/alar2
 ./bin/alar2 $1
+# Save the error state of from alar2
+error_state=$?
 Log-Output "Recovery script finished"
+exit $error_state
