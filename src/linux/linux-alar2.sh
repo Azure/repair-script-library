4f92fa16303c56bf924d663cef6a8562f08cd7b1
malachma <malachma@microsoft.com>
2021-03-05 19:03:33 +0100

    added build commands to the script. alar2 will be build on the repair VM only

--- src/linux/linux-alar2.sh
+++ src/linux/linux-alar2.sh
@@ -3,6 +3,9 @@
 
 Log-Output "Starting the recovery"
 cd ./src/linux/common/helpers/alar2
+cargo build --release
+mkdir bin
+cp target/release/alar2 bin/
 chmod 700 ./bin/alar2
 ./bin/alar2 $1
 # Save the error state of from alar2
