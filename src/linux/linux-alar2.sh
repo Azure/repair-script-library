314175766a87fb60c64968ac02a2e2727be11c3b
malachma <malachma@microsoft.com>
2021-03-05 20:09:42 +0100

    errors to /dev/null

--- src/linux/linux-alar2.sh
+++ src/linux/linux-alar2.sh
@@ -3,12 +3,12 @@
 
 Log-Output "Starting the recovery"
 cd ./src/linux/common/helpers/alar2
-/root/.cargo/bin/cargo build --release
+/root/.cargo/bin/cargo build --release 2>&1 > /dev/null
 mkdir bin
 cp target/release/alar2 bin/
 chmod 700 ./bin/alar2
 ./bin/alar2 $1
-# Save the error state of from alar2
+# Save the error state from alar2
 error_state=$?
 Log-Output "Recovery script finished"
 exit $error_state
