3e445becad64a21de8f92def5bb5374449f1616c
malachma <malachma@microsoft.com>
2021-03-05 23:40:27 +0100

    set quiet option for cargo

--- src/linux/linux-alar2.sh
+++ src/linux/linux-alar2.sh
@@ -3,7 +3,7 @@
 
 Log-Output "Starting the recovery"
 cd ./src/linux/common/helpers/alar2
-/root/.cargo/bin/cargo build --release 2>&1 > /dev/null
+/root/.cargo/bin/cargo build -q --release 
 mkdir bin
 cp target/release/alar2 bin/
 chmod 700 ./bin/alar2
