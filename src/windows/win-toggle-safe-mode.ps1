354a50d17447c74fb1c86b7d3219afef877abf18
Ryan McCallum <rjmccallumbigl@gmail.com>
2021-03-20 12:27:17 -0400

    Add try-catch, modify syntax

--- src/windows/win-toggle-safe-mode.ps1
+++ src/windows/win-toggle-safe-mode.ps1
@@ -7,8 +7,8 @@
 # users may find their VM booting into Safe Mode inadvertantly due to user error or misconfiguration,
 # which will disable RDP access until corrected. This script utilizes the az vm repair extension to 
 # clone the VM into a Hyper-V environment using Nested Virtualization and toggle Safe Boot. The user
-# may then access their VM in Safe Mode via the Rescue VM or revert Safe Mode on their Azure VM. They may
-# then swap the disk using the `az vm repair restore` functionality.
+# may then access their VM in Safe Mode via the Rescue VM or revert Safe Mode on their Azure VM. They 
+# may then swap the disk using the `az vm repair restore` functionality.
 #
 # https://docs.microsoft.com/en-us/cli/azure/ext/vm-repair/vm/repair?view=azure-cli-latest
 # https://docs.microsoft.com/en-us/troubleshoot/azure/virtual-machines/troubleshoot-rdp-safe-mode
@@ -24,109 +24,104 @@ $logFile = "$env:PUBLIC\Desktop\$($scriptName).log"
 Log-Info 'START: Running Script win-toggle-safe-mode' | out-file -FilePath $logFile -Append
 $scriptStartTime | out-file -FilePath $logFile -Append
 
-# Make sure guest VM is shut down
-$guestHyperVVirtualMachine = Get-VM
-Log-Info "#01 - Stopping nested guest VM $guestHyperVVirtualMachine.VMName" | out-file -FilePath $logFile -Append
-$return = Stop-VM $guestHyperVVirtualMachine -ErrorAction Stop -Force
+try {
 
-# Make sure the disk is online
-Log-Info "#02 - Bringing disk online" | out-file -FilePath $logFile -Append
-$disk = get-disk -ErrorAction Stop | where {$_.FriendlyName -eq 'Msft Virtual Disk'}
-$return = $disk | set-disk -IsOffline $false -ErrorAction Stop
+    # Make sure guest VM is shut down
+    $guestHyperVVirtualMachine = Get-VM
+    Log-Info "#01 - Stopping nested guest VM $guestHyperVVirtualMachine.VMName" | out-file -FilePath $logFile -Append
+    $return = Stop-VM $guestHyperVVirtualMachine -ErrorAction Stop -Force
+
+    # Make sure the disk is online
+    Log-Info "#02 - Bringing disk online" | out-file -FilePath $logFile -Append
+    $disk = get-disk -ErrorAction Stop | where { $_.FriendlyName -eq 'Msft Virtual Disk' }
+    $return = $disk | set-disk -IsOffline $false -ErrorAction Stop
  
-# Handle disk partitions
-$partitionlist = Get-Disk-Partitions
-Log-Info $partitionlist | out-file -FilePath $logFile -Append
-$partitionGroup = $partitionlist | group DiskNumber | out-file -FilePath $logFile -Append
+    # Handle disk partitions
+    $partitionlist = Get-Disk-Partitions
+    Log-Info $partitionlist | out-file -FilePath $logFile -Append
+    $partitionGroup = $partitionlist | group DiskNumber | out-file -FilePath $logFile -Append
 
-Log-Info '#03 - enumerate partitions to reconfigure boot cfg' | out-file -FilePath $logFile -Append
+    Log-Info '#03 - enumerate partitions for boot config' | out-file -FilePath $logFile -Append
 
-forEach ( $partitionGroup in $partitionlist | group DiskNumber )
-{
-    #reset paths for each part group (disk)
-    $isBcdPath = $false
-    $bcdPath = ''
-    $bcdDrive = ''
-    $isOsPath = $false
-    $osPath = ''
-    $osDrive = ''
+    forEach ( $partitionGroup in $partitionlist | group DiskNumber ) {
+        # Reset paths for each part group (disk)
+        $isBcdPath = $false
+        $bcdPath = ''
+        $bcdDrive = ''
+        $isOsPath = $false
+        $osPath = ''
+        $osDrive = ''
 
-    #scan all partitions of a disk for bcd store and os file location 
-    ForEach ($drive in $partitionGroup.Group | select -ExpandProperty DriveLetter )
-    {      
-        #check if no bcd store was found on the previous partition already
-        if ( -not $isBcdPath )
-        {
-            $bcdPath =  $drive + ':\boot\bcd'
-            $bcdDrive = $drive + ':'
-            $isBcdPath = Test-Path $bcdPath
-
-            #if no bcd was found yet at the default location look for the uefi location too
-            if ( -not $isBcdPath )
-            {
-                $bcdPath =  $drive + ':\efi\microsoft\boot\bcd'
+        # Scan all partitions of a disk for bcd store and os file location 
+        ForEach ($drive in $partitionGroup.Group | select -ExpandProperty DriveLetter ) {      
+            # Check if no bcd store was found on the previous partition already
+            if ( -not $isBcdPath ) {
+                $bcdPath = $drive + ':\boot\bcd'
                 $bcdDrive = $drive + ':'
                 $isBcdPath = Test-Path $bcdPath
-            } 
-        }        
 
-        #check if os loader was found on the previous partition already
-        if (-not $isOsPath)
-        {
-            $osPath = $drive + ':\windows\system32\winload.exe'
-            $isOsPath = Test-Path $osPath
-            if ($isOsPath)
-            {
-                $osDrive = $drive + ':'
+                # If no bcd was found yet at the default location look for the uefi location too
+                if ( -not $isBcdPath ) {
+                    $bcdPath = $drive + ':\efi\microsoft\boot\bcd'
+                    $bcdDrive = $drive + ':'
+                    $isBcdPath = Test-Path $bcdPath
+                } 
+            }        
+
+            # Check if os loader was found on the previous partition already
+            if (-not $isOsPath) {
+                $osPath = $drive + ':\windows\system32\winload.exe'
+                $isOsPath = Test-Path $osPath
+                if ($isOsPath) {
+                    $osDrive = $drive + ':'
+                }
             }
         }
-    }
-        #if both was found grab bcd store
-    if ( $isBcdPath -and $isOsPath )
-    {
-        # Get Safe Mode State
-        Log-Info "#04 - Checking safeboot flag for $bcdPath" | out-file -FilePath $logFile -Append
-        $bcdout = bcdedit /store $bcdPath /enum
-        $defaultLine = $bcdout | Select-String 'displayorder' | select -First 1
-        $defaultId = '{'+$defaultLine.ToString().Split('{}')[1] + '}'
-        $safeModeIndicator = $bcdout | Select-String 'safeboot' | select -First 1
+        # If both was found grab bcd store
+        if ( $isBcdPath -and $isOsPath ) {
+            # Get Safe Mode state
+            Log-Info "#04 - Checking safeboot flag for $bcdPath" | out-file -FilePath $logFile -Append
+            $bcdout = bcdedit /store $bcdPath /enum
+            $defaultLine = $bcdout | Select-String 'displayorder' | select -First 1
+            $defaultId = '{' + $defaultLine.ToString().Split('{}')[1] + '}'
+            $safeModeIndicator = $bcdout | Select-String 'safeboot' | select -First 1
 
-        # Check if flag exists
-        if ($safeModeIndicator)
-        {            
-            
-            # Flag exists, delete to take VM out of Safe Mode
-            Log-Info "#05 - Removing safeboot flag for $bcdPath" | out-file -FilePath $logFile -Append
-            bcdedit /store $bcdPath /deletevalue $defaultId safeboot
-         
-        } else {
-            
-            # Flag doesn't exist, adding so VM boots in Safe Mode
-            Log-Info "#05 - Configuring safeboot flag for $bcdPath" | out-file -FilePath $logFile -Append
-            bcdedit /store $bcdPath /set $defaultId safeboot network
+            # Check if flag exists
+            if ($safeModeIndicator) {                        
+                # Flag exists, delete to take VM out of Safe Mode
+                Log-Info "#05 - Removing safeboot flag for $bcdPath" | out-file -FilePath $logFile -Append
+                bcdedit /store $bcdPath /deletevalue $defaultId safeboot         
+            }
+            else {            
+                # Flag doesn't exist, adding so VM boots in Safe Mode
+                Log-Info "#05 - Configuring safeboot flag for $bcdPath" | out-file -FilePath $logFile -Append
+                bcdedit /store $bcdPath /set $defaultId safeboot network
+            }
+            # Bring disk offline 
+            Log-Info "#06 - Bringing disk offline" | out-file -FilePath $logFile -Append
+            $return = $disk | set-disk -IsOffline $true -ErrorAction Stop
+
+            # Start Hyper-V VM
+            Log-Info "END: Starting VM, please verify Safe Mode w/ Networking using MSCONFIG.exe" | out-file -FilePath $logFile -Append
+            $return = start-vm $guestHyperVVirtualMachine -ErrorAction Stop
+
+            return $STATUS_SUCCESS
         }
-
-        # Bring disk offline 
-        Log-Info "Bringing disk offline" | out-file -FilePath $logFile -Append
-        $return = $disk | set-disk -IsOffline $true -ErrorAction Stop
-
-        # Start Hyper-V VM
-        Log-Info "END: Starting VM, please verify Safe Mode w/ Networking using MSCONFIG.exe" | out-file -FilePath $logFile -Append
-        $return = start-vm $guestHyperVVirtualMachine -ErrorAction Stop
-
-        return $STATUS_SUCCESS
     }
 }
+catch {
+    
+    # Log failure to run successfully
+    Log-Error "ERROR: Unable to find the BCD Path" | out-file -FilePath $logFile -Append
 
-# Log failure to run successfully
-Log-Error "Unable to find the BCD Path" | out-file -FilePath $logFile -Append
+    # Bring disk offline again
+    Log-Info "#05 - Bringing disk offline" | out-file -FilePath $logFile -Append
+    $return = $disk | set-disk -IsOffline $true -ErrorAction Stop
 
-# Bring disk offline again
-Log-Info "Bringing disk offline" | out-file -FilePath $logFile -Append
-$return = $disk | set-disk -IsOffline $true -ErrorAction Stop
+    # Start Hyper-V VM again
+    Log-Info "END: could not start Safe Mode, BCD store may need to be repaired" | out-file -FilePath $logFile -Append
+    $return = start-vm $guestHyperVVirtualMachine -ErrorAction Stop
 
-# Start Hyper-V VM again
-Log-Info "END: could not start Safe Mode, BCD store may need to be repaired" | out-file -FilePath $logFile -Append
-$return = start-vm $guestHyperVVirtualMachine -ErrorAction Stop
-
-return $STATUS_ERROR
+    throw $_
+    return $STATUS_ERROR
+}
