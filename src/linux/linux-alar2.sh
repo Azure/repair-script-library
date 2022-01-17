8f4ab8a0997d106369a4bd99b5120ba1cfd3775d
malachma <malachma@microsoft.com>
2022-01-17 11:17:51 +0100

    by default the pre-build binaryis used

--- src/linux/linux-alar2.sh
+++ src/linux/linux-alar2.sh
@@ -5,11 +5,16 @@
 apt-get install libclang-dev -y
 Log-Output "Starting the recovery"
 cd ./src/linux/common/helpers/alar2
-/root/.cargo/bin/cargo build -q --release 
-mkdir bin
-cp target/release/alar2 bin/
-chmod 700 ./bin/alar2
-./bin/alar2 $1
+if [[ -f target/debug/alar2 ]]; then
+    chmod 700 target/debug/alar2
+    export RUST_BACKTRACE=1
+    target/debug/alar2 $1
+else
+    /root/.cargo/bin/cargo build -q  
+    chmod 700 target/debug/alar2
+    export RUST_BACKTRACE=1 
+    target/debug/alar2 $1
+fi
 # Save the error state from alar2
 error_state=$?
 Log-Output "Recovery script finished"
