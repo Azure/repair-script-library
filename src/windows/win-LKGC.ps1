ac779447dfa9de0638c209ee769045f34274382a
Daniel Muñoz L <jotmeil.com@hotmail.com>
2022-06-12 13:49:56 -0600

    Update win-LKGC.ps1
    Shrank script.
    Works effectively less prone to failures even when re-ran more than once in same rescue VM
    Finds for all letters available for windows assignments.
    Shows message according to failures or when succeeds.

--- src/windows/win-LKGC.ps1
+++ src/windows/win-LKGC.ps1
@@ -1,57 +1,37 @@
-# Contact me Daniel Muñoz L : damunozl@microsoft.com if support is required.
-# Echo off null outputs
+# Welcome to the AUTO LKGC(Last Known Good Configuration) by Daniel Muñoz L!
+# Contact me Daniel Muñoz L : damunozl@microsoft.com if questions.
 out-null
-
-# Change color
 cmd /c color 0A
-
-# Change title of console
-$title="                                                                                  --== AUTO LKGC by Daniel Muñoz L ==--"
-$host.UI.RawUI.WindowTitle = $Title
-
-# "Welcome to the AUTO LKGC(Last Known Good Configuration) by Daniel Muñoz L!"
+$host.UI.RawUI.WindowTitle = "                                                                                  --== AUTO LKGC by Daniel Muñoz L ==--"
 
 # Rescue OS variable
 $diska='c'
 
 # Finder for faulty OS letter
-if (Test-Path -Path 'l:\Windows') {
-  $diskb='l'
-} else {
-if (Test-Path -Path 'i:\Windows') {
-  $diskb='i'
-} else {
-if (Test-Path -Path 'g:\Windows') {
-  $diskb='g'
-} else {
-if (Test-Path -Path 'j:\Windows') {
-  $diskb='j'
-} else {
-if (Test-Path -Path 'k:\Windows') {
-  $diskb='k'
-} else {
-if (Test-Path -Path 'f:\Windows') {
-  $diskb='f'
-} else {
-if (Test-Path -Path 'h:\Windows') {
-  $diskb='h'
-} else {	
-"Path doesn't exist."
-}}}}}}}
+$diskarray = "d","q","w","e","r","t","y","u","i","o","p","s","f","g","h","j","k","l","z","x","v","n","m"
+$diskb="000"
+foreach ($diskt in $diskarray)
+{
+   if (Test-Path -Path "$($diskt):\Windows")
+   {
+    $diskb=$diskt
+    } 
+}
 
-# cmd version
-# for /f "tokens=3 " %a in ('reg query "HKU\BROKENSYSTEM\select" /v "current" ^|findstr /ri "DWORD"') do echo=%a
-# for /f "tokens=4 delims=x " %a in ('reg query "HKU\BROKENSYSTEM\select" /v "current" ^|findstr /ri "DWORD"') do echo=%a
-# for /f "tokens=4 delims=x " %a in ('reg query "HKU\BROKENSYSTEM\select" /v "current" ^|findstr /ri "DWORD"') do set current=%a
+# IN CASE OF FINDER FAILURE
+if ($diskb -eq "000") {write-output "SCRIPT COULD NOT FIND A RESCUE OS DISK ATTACHED, EXITING";start-sleep 10;Exit}
 
 # OS VER PEEK
-reg load "HKLM\BROKENSYSTEM" "$($diskb):\Windows\System32\config\software"
-(Get-ItemProperty -path 'registry::hklm\BROKENSYSTEM\microsoft\windows nt\currentversion').ProductName | %{ [int]$winosver=$_.Split(' ')[1]; }
-(Get-ItemProperty -path 'registry::hklm\BROKENSYSTEM\microsoft\windows nt\currentversion').ProductName | %{ [int]$winosver=$_.Split(' ')[2]; }
+reg.exe load "HKLM\BROKENSYSTEM" "$($diskb):\Windows\System32\config\software"
+Start-sleep 3
+(Get-ItemProperty -path 'registry::hklm\BROKENSYSTEM\microsoft\windows nt\currentversion').ProductName | %{ [int]$winosver=$_.Split(' ')[1]; } 2> $null
+(Get-ItemProperty -path 'registry::hklm\BROKENSYSTEM\microsoft\windows nt\currentversion').ProductName | %{ [int]$winosver=$_.Split(' ')[2]; } 2> $null
 reg.exe unload "HKLM\BROKENSYSTEM"
+Start-sleep 3
 
 # Hive loader into rescue VM
-reg load "HKU\BROKENSYSTEM" "$($diskb):\Windows\System32\config\SYSTEM"
+reg.exe load "HKU\BROKENSYSTEM" "$($diskb):\Windows\System32\config\SYSTEM"
+Start-sleep 3
 
 # Acquiring reg values
 $currentreg = (Get-ItemProperty -path Registry::HKU\BROKENSYSTEM\Select).current
@@ -62,20 +42,20 @@ $lkgcreg = (Get-ItemProperty -path Registry::HKU\BROKENSYSTEM\Select).LastKnownG
 # FILTER IF LKGC IS ALREADY THERE FOR Windows 10, Windows Server 2016, and newer versions.
 if (($winosver -eq 10) -or ($winosver -ge 2016)) 
 {
-if ($currentreg -ge 2) {reg.exe unload "HKU\BROKENSYSTEM";exit}
-elseif ($defaultreg -ge 2) {reg.exe unload "HKU\BROKENSYSTEM";exit}
-elseif ($failedreg -ge 1) {reg.exe unload "HKU\BROKENSYSTEM";exit}
-elseif ($lkgcreg -ge 2) {reg.exe unload "HKU\BROKENSYSTEM";exit}
+if ($currentreg -ge 2) {reg.exe unload "HKU\BROKENSYSTEM";write-output "LKGC WAS ALREADY SET, NO CHANGES DONE";start-sleep 5;exit}
+elseif ($defaultreg -ge 2) {reg.exe unload "HKU\BROKENSYSTEM";write-output "LKGC WAS ALREADY SET, NO CHANGES DONE";start-sleep 5;exit}
+elseif ($failedreg -ge 1) {reg.exe unload "HKU\BROKENSYSTEM";write-output "LKGC WAS ALREADY SET, NO CHANGES DONE";start-sleep 5;exit}
+elseif ($lkgcreg -ge 2) {reg.exe unload "HKU\BROKENSYSTEM";write-output "LKGC WAS ALREADY SET, NO CHANGES DONE";start-sleep 5;exit}
 }
 
 # FILTER IF LKGC IS ALREADY THERE FOR Windows Server 2012 version.
 if ($winosver -eq 2012) 
 {
-# CHECK IF LKGC IS ALREADY THERE
-if ($currentreg -ge 2) {reg.exe unload "HKU\BROKENSYSTEM";exit}
-elseif ($defaultreg -ge 2) {reg.exe unload "HKU\BROKENSYSTEM";exit}
-elseif ($failedreg -ge 1) {reg.exe unload "HKU\BROKENSYSTEM";exit}
-elseif ($lkgcreg -ge 3) {reg.exe unload "HKU\BROKENSYSTEM";exit}
+# CHECK IF LKGC IS ALREADY THERE AND EXIT IF SO
+if ($currentreg -ge 2) {reg.exe unload "HKU\BROKENSYSTEM";write-output "LKGC WAS ALREADY SET, NO CHANGES DONE";start-sleep 5;exit}
+elseif ($defaultreg -ge 2) {reg.exe unload "HKU\BROKENSYSTEM";write-output "LKGC WAS ALREADY SET, NO CHANGES DONE";start-sleep 5;exit}
+elseif ($failedreg -ge 1) {reg.exe unload "HKU\BROKENSYSTEM";write-output "LKGC WAS ALREADY SET, NO CHANGES DONE";start-sleep 5;exit}
+elseif ($lkgcreg -ge 3) {reg.exe unload "HKU\BROKENSYSTEM";write-output "LKGC WAS ALREADY SET, NO CHANGES DONE";start-sleep 5;exit}
 }
 
 # Plus 1
@@ -84,7 +64,7 @@ $defaultreg = $defaultreg+1
 $failedreg = $failedreg+1
 $lkgcreg = $lkgcreg+1
 
-# Adding new registries to enable LKGC
+# ENABLING LKGC
 Set-Itemproperty -path Registry::HKU\BROKENSYSTEM\Select -Name 'current' -Type DWORD -value $currentreg
 Set-Itemproperty -path Registry::HKU\BROKENSYSTEM\Select -Name 'default' -Type DWORD -value $defaultreg
 Set-Itemproperty -path Registry::HKU\BROKENSYSTEM\Select -Name 'failed' -Type DWORD -value $failedreg
@@ -92,3 +72,6 @@ Set-Itemproperty -path Registry::HKU\BROKENSYSTEM\Select -Name 'LastKnownGood' -
 
 # Unload Hive
 reg.exe unload "HKU\BROKENSYSTEM"
+
+write-output "          ---------------          SCRIPT FINISHED PROPERLY, LKGC APPLIED          ---------------          "
+start-sleep 10
