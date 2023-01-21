25c85e8f02cc55d24e28d2d9a869a90258701060
malachma <malachma@microsoft.com>
2023-01-21 23:30:47 +0100

    testing new functionality

--- src/linux/linux-alar2.sh
+++ src/linux/linux-alar2.sh
@@ -5,19 +5,10 @@
 apt-get update
 apt-get install libclang-dev -y
 
-Log-Output "Starting the recovery"
-cd ./src/linux/common/helpers/alar2
-if [[ -f target/debug/alar2 ]]; then
-    chmod 700 target/debug/alar2
-    export RUST_BACKTRACE=1
-    target/debug/alar2 $1
-else
-    /root/.cargo/bin/cargo build -q  
-    chmod 700 target/debug/alar2
-    export RUST_BACKTRACE=1 
-    target/debug/alar2 $1
-fi
-# Save the error state from alar2
-error_state=$?
-Log-Output "Recovery script finished"
-exit $error_state
+Log-Output "Starting ALAR"
+wget https://raw.githubusercontent.com/Azure/ALAR/main/src/run-alar.sh
+chmod 700 run-alar.sh
+./run-alar.sh $1
+
+Log-Output "ALAR stopped"
+exit $?
