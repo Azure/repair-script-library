4d903b518c841a88ab8b28215319b9c2c21ec7a6
malachma <malachma@microsoft.com>
2023-01-28 21:51:30 +0100

    rmeove the dependencies. such things are all handled by run-alar.sh

--- src/linux/linux-alar2.sh
+++ src/linux/linux-alar2.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# libclang needs to be installed as well, due to a new dependency
-apt-get update
-apt-get install libclang-dev -y
-
 Log-Output "Starting ALAR"
 wget https://raw.githubusercontent.com/Azure/ALAR/main/src/run-alar.sh
 chmod 700 run-alar.sh
