645fadf02bc724ff3ca8ca398a77bce6032b4c7b
Mingyang Zheng <mizhe@microsoft.com>
2024-07-21 16:04:52 -0700

    Update win-crowdstrike-fix-bootloop-v2.ps1

--- src/windows/win-crowdstrike-fix-bootloop-v2.ps1
+++ src/windows/win-crowdstrike-fix-bootloop-v2.ps1
@@ -14,43 +14,33 @@ forEach ( $partition in $partitionlist )
         Remove-Item $corruptFiles
         Log-Info "Corrupt crowdstrike files are removed."
 
-        Log-Info "Load/unload registry hives from data disk..."
-
-        $result = reg load HKLM\temp_system_hiv $driverletter\windows\system32\config\system
-        if ($LASTEXITCODE -ne 0) {
-            Log-Error "Load registry hive from $driverletter\windows\system32\config\system failed with error: $result"
-            return $STATUS_ERROR
-        } else {
-            Log-Info "Load registry hive from $driverletter\windows\system32\config\system succeeded: $result"
+        Log-Info "Loading/unloading registry hives from data disk..."
+        $guidSuffix = "f85afa50-13cc-48e0-8a29-90603a43cfe2" # get a guid online as the reg key suffix in case the reg key name already exist
+        $regKeyToFile = @{
+            "HKLM\temp_system_hive_$guidSuffix" = "$driverletter\windows\system32\config\system"
+            "HKLM\temp_software_hive_$guidSuffix" = "$driverletter\windows\system32\config\software"
         }
 
-        $result = reg unload HKLM\temp_system_hiv
-        if ($LASTEXITCODE -ne 0) {
-            Log-Error "Unload registry hive HKLM\temp_system_hiv failed with error: $result"
-            return $STATUS_ERROR
-        } else {
-            Log-Info "Unload registry hive HKLM\temp_system_hiv succeeded: $result"
-        }
- 
-        $result = reg load HKLM\temp_software_hive $driverletter\windows\system32\config\software
-        if ($LASTEXITCODE -ne 0) {
-            Log-Error "Load registry hive from $driverletter\windows\system32\config\software failed with error: $result"
-            return $STATUS_ERROR
-        } else {
-            Log-Info "Load registry hive from $driverletter\windows\system32\config\software succeeded: $result"
-        }
-
-        $result = reg unload HKLM\temp_software_hive 
-        if ($LASTEXITCODE -ne 0) {
-            Log-Error "Unload registry hive HKLM\temp_software_hive failed with error: $result"
-            return $STATUS_ERROR
-        } else {
-            Log-Info "Unload registry hive HKLM\temp_software_hive succeeded: $result"
+        foreach ($regKey in $regKeyToFile.Keys)
+        {
+            $regFile = $regKeyToFile[$regKey]
+            Log-Info "Loading registry hive $regKey from $regFile..."
+            $result = reg load $regKey $regFile 2>&1
+            if ($LASTEXITCODE -ne 0) {
+                Log-Error "Load registry hive $regKey from $regFile failed with error: $result"
+            } else {
+                Log-Info "Load registry hive $regKey from $regFile succeeded with message: $result"
+                Log-Info "Unloading registry hive $regKey..."
+                $result = reg unload $regKey 2>&1
+                if ($LASTEXITCODE -ne 0) {
+                    Log-Error "Unload registry hive $regKey failed with error: $result"
+                } else {
+                    Log-Info "Unload registry hive $regKey succeeded with message: $result"
+                }
+            }
         }
 
         Log-Info "Registry hives load/unload: done."
-
-
         $actionTaken = $true
     }
 }
