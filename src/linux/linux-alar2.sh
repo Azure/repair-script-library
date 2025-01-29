2fa8f561a15361986c5156d8644c0aa95e104a49
Adam Sandor <adsandor@microsoft.com>
2025-01-29 14:37:04 -0500

    Update linux-alar2.sh

--- src/linux/linux-alar2.sh
+++ src/linux/linux-alar2.sh
@@ -1,3 +1,25 @@
+﻿#########################################################################################################
+<#
+# .SYNOPSIS
+#   alar2 allows recovering a failed VM. Various actions are available like: fstab, initrd, and kernel. NOTE: use option --run-on-repair. 
+# 
+# .DESCRIPTION
+#   Runs a workflow from https://github.com/Azure/ALAR/tree/main/src/action_implementation depending on the parameter included when ran. 
+#   For instance, running with az vm repair run --verbose -g RGNAME -n VMNAME --run-id linux-alar2 --parameters fstab --run-on-repair 
+#   will run the fstab workflow to correct fstab issues. 
+#   Public doc: https://learn.microsoft.com/en-us/troubleshoot/azure/virtual-machines/linux/repair-linux-vm-using-alar
+# 
+# .RESOLVES
+#   malformed /etc/fstab
+#   damaged initrd or /boot/grub/grub.cfg is missing the right setup
+#   last installed kernel is not bootable
+#   serial console and grub serial are not configured well
+#   GRUB/EFI installation or configuration damaged
+#   Disk full causing a non-boot scenario, specifically related to auditd configurations.
+#>
+#########################################################################################################
+
+
 #!/bin/bash
 
 wget https://raw.githubusercontent.com/Azure/ALAR/main/src/run-alar.sh
