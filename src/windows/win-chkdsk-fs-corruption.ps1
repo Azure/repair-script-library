2b2d0e030af9a918298294043f84e22885decb16
Adam Sandor <adsandor@microsoft.com>
2025-01-29 15:24:03 -0500

    Update win-chkdsk-fs-corruption.ps1

--- src/windows/win-chkdsk-fs-corruption.ps1
+++ src/windows/win-chkdsk-fs-corruption.ps1
@@ -1,3 +1,13 @@
+# .SUMMARY
+#   Runs chkdsk to fix file system corruption.
+#   Checks if dirty bit has been set and if so, runs a chkdsk.exe on the attached disk.
+#   Public doc: https://learn.microsoft.com/en-us/troubleshoot/azure/virtual-machines/windows/troubleshoot-check-disk-boot-error
+# 
+# .RESOLVES
+#   A Windows VM doesn't start. When you check the boot screenshots in Boot diagnostics, you see that the Check Disk process (chkdsk.exe)
+#   is running with one of the following messages: 1. Scanning and repairing drive (C:) , 2. Checking file system on C: .
+#   If an NTFS error is found in the file system, the dirty bit will set and the disk check application will run to try and fix any corruption. Running it from a rescue VM helps prevent interruptions.
+
 . .\src\windows\common\setup\init.ps1
 . .\src\windows\common\helpers\Get-Disk-Partitions.ps1
 
