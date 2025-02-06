afde149a9821eb35edf7899c38de436a7247b28c
Adam Sandor <adsandor@microsoft.com>
2025-02-06 13:40:56 -0500

    Update win-crowdstrike-fix-bootloop-v2.ps1

--- src/windows/win-crowdstrike-fix-bootloop-v2.ps1
+++ src/windows/win-crowdstrike-fix-bootloop-v2.ps1
@@ -1,3 +1,16 @@
+<#
+# .SUMMARY
+#   Most recent version of this fix. Workaround for machines stuck in boot loop due to corrupt crowdstrike falcon sensor 2024-07-19 by removing corrupt crowdstrike files, 
+#   loading/unloading the registry hives, and removing regtrans-ms and txr.blf files under config\TxR folder.
+#   Check for the corrupt CrowdStrike driver file(s) C-00000291*.sys. If found, delete it. Load the registry hive to check for corruption and if corruption is found, fixes it.
+# 
+# .RESOLVES
+#   Helps recover VMs stuck in a non-boot state due to the faulty CrowdStrike update from July 19th, 2024.
+#
+# .PUBLIC DOCS
+#   https://techcommunity.microsoft.com/blog/azurecompute/recovery-options-for-azure-virtual-machines-vm-affected-by-crowdstrike-falcon-ag/4196798
+#>
+
 . .\src\windows\common\setup\init.ps1
 . .\src\windows\common\helpers\Get-Disk-Partitions-v2.ps1
 
